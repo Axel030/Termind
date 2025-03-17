59a02497801f71568796c6839de2ebfc130340eb
Marco Cifuentes <escobarcifuentesantoniomarco@gmail.com>
2025-03-17 16:44:23 -0600

    Validaciones de base de datos

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 console.clear(); // Limpia la consola al ejecutar cambios en tiempo real
+const { error } = require('console');
 // para init el server usar npm run dev
 // para dejar de init el server con Crtl + c
 const express = require('express');
 const { Client } = require('pg');
-
+const query = "SELECT * FROM Alumno"
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -11,16 +12,17 @@ app.use(express.json());
 const client = new Client({
     user: 'postgres', // user default si no cambiar
     host: 'localhost',// host igualemte default 
-    database: 'scolegio',// si la db tiene otro nombre cambiar 
-    password: '1234',// siempre poner su password
+    database: 'Scolegio',// si la db tiene otro nombre cambiar 
+    password: 'marcogh3141.',// siempre poner su password
     port: 5432// port es igual default si no cambiar
 });
 client.connect()
     .then(() => console.log("Conexión a la base de datos exitosa"))
     .catch(error => console.error("Error en la conexión a la base de datos", error));
 
-app.get('/informacion', async (req, res) => {
+app.get('/info', async (req, res) => {
     console.log("Alguien entró al servidor /informacion");
+    res.send("HOLa gente")
     try {
         const result = await client.query('SELECT * FROM alumno');
         res.status(200).json(result.rows);
@@ -36,3 +38,47 @@ app.listen(PORT, (error) => {
         console.log("error server");
     }
 });
+
+
+const user_login =  async(req, res)=>{
+    try{
+        let{user, password } =  req.body
+        const users = await client.query("SELECT usuario, contraseña FROM Usuarios WHERE usuario = $1",[user            
+        ]);
+
+        if(!users.rows.length === 1){
+            res.status(409).json({
+                error: "lo siento su usario no existe",
+              });
+        }
+        else if(password == users.rows[0].contraseña){
+                res.json({
+                    message : "Inicio de sesion exitoso "
+                })  
+        }
+        else{
+            res.status(401).json({
+                error : "Error al incio de sesion"
+            })
+        }
+    } catch(error){
+        console.log(error.message);
+        res.status(500).json({
+            error : error.message,
+        });
+
+    }
+}
+const validate = module.exports = (req, res, next) => {
+    const { user, password } = req.body;
+    if(req.path === "/login"){
+        if(!user || !password){
+            return res.status(400).json({
+                error : "Faltan credenciales "
+            });
+        }
+    }
+    next();
+}
+
+app.post("/login", validate, user_login);
